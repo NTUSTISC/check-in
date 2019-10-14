263c8b1be022fc49e1ea76734218577f8dc878af
segnolin <segnolin@gmail.com>
2019-10-14 23:34:21 +0800

    more fun

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false, scanPeriod: 5 });
 scanner.addListener('scan', function (content) {
-  var token = content.substr(0, 40);
+  var token = content.split('#')[0];
   console.log(token);
   document.getElementById('token').innerHTML = token; // XSS me
   var output = httpGet('https://script.google.com/macros/s/AKfycbwIy1RuKcGxtUI0caTyLC9LZia6m3gVdKrPXQAYU8cw7el_MBs/exec?token=' + token);
